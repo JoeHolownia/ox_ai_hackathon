aa2c8f7e29ae67989ae6cb2cb447567bb3f00085
JoeHolownia <joeholownia7@gmail.com>
2024-11-10 19:31:19 +0000

    Successfully got chatbot working again after removing for design

--- app.py
+++ app.py
@@ -43,10 +43,15 @@ create_session_body = {
     "pluginIds": [],
     "externalUserId": external_user_id
 }
+ 
 
 # Make the request to create a chat session
 response = requests.post(create_session_url, headers=create_session_headers, json=create_session_body)
 response_data = response.json()
+
+# Extract session ID from the response
+session_id = response_data['data']['id']
+
 # Meal date input (user selects the day of the meal) with max_date set to today
 meal_date = st.date_input("", datetime.today(), max_value=datetime.today())
 
@@ -81,24 +86,89 @@ with tab1:
         if uploaded_image:
             st.image(uploaded_image, caption="Uploaded Food Image", use_container_width=True)  # Updated to use_container_width
 
+    if uploaded_image:
+
+        # Define where to save the image
+        save_path = f"./food_image.jpg"
+        
+        # Save the file
+        with open(save_path, "wb") as file:
+            file.write(uploaded_image.getbuffer())
+        
+        # st.success(f"Image saved at: {save_path}")
+
+        # # Convert the uploaded image to binary
+        image_data = uploaded_image.read()
+
+        files = {
+            "file": image_data
+        }
+
+        # st.write(f"{os.path.getsize(save_path)}")
+
+        # img body data
+        submit_img_body = {
+            "createdBy": "Joe Holownia",
+            "updatedBy": "Joe Holownia",
+            "sessionId": f"{session_id}",
+            "name": "image_of_food",
+            "plugins": ["plugin-1712327325", "plugin-1713962163"],
+            "sizeBytes":  7834093,
+            "responseMode": "sync"
+        }             
+
+        # submit image to image api
+        try:
+
+            response = requests.post(submit_img_url, 
+                                    headers=submit_img_header, 
+                                    files=files, 
+                                    data=submit_img_body)
+
+            # To print the response
+            # st.write(response.status_code)
+            # st.write(response.json())
+        except Exception as e:
+            st.write("Error:", e)
 
     st.markdown(
         "<h4 style='text-align: center;'>Ask Nutrify about your food:</h4>", 
         unsafe_allow_html=True
     )
+
     # Chatbot-like input box with placeholder text
     user_input = st.text_input("", placeholder="Ask me a question about the food")
 
     # Simulate chatbot output (you can connect to an actual chatbot API later)
     chatbot_output = ""
     if user_input and uploaded_image:
+        
         # Display the user query
         st.write(f"User asked: {user_input}")
+
+        # chat query body data
+        submit_query_body = {
+            "endpointId": "predefined-openai-gpt4o",
+            "query": f"{user_input}",
+            "pluginIds": ["plugin-1712327325", "plugin-1713962163"],
+            "responseMode": "sync"
+        }
+
+        try:
+            response = requests.post(submit_query_url,
+                                    headers=submit_query_headers,
+                                    json=submit_query_body
+                                    )
+            if response.status_code == 200:
+                result = response.json()
+                print(result)
+                # Display the chatbot's response
+                st.write("Chatbot response:")
+                st.write(result['data']['answer'])
         
-        # Mock API call for chatbot response
-        # (In a real implementation, send the image and user input to a chatbot API)
-        chatbot_output = f"Chatbot response: Here is the nutritional information for your meal. {user_input}"
-        st.write(chatbot_output)
+        except Exception as e:
+            st.write("Error:", e)
+
 
 with tab2:
     # --- Health Symptoms Text Box ---
@@ -123,93 +193,6 @@ if st.button("Submit"):
     else:
         st.error("Please make sure to fill in all the fields (image, symptoms, and questions).")
 
-# st.subheader("Tap the camera icon below to upload an image of your food:")
-
-# Custom camera icon button for file upload
-col1, col2, col3 = st.columns([1, 3, 1])
-with col2:
-    # Display the camera icon as a button
-    uploaded_image = st.file_uploader("", type=["jpg", "png", "jpeg"], key="image_uploader")
-
-    if uploaded_image:
-        st.image(uploaded_image, caption="Uploaded Food Image", use_container_width=True)  # Updated to use_container_width
-
-if uploaded_image:
-
-    # Define where to save the image
-    save_path = f"./food_image.jpg"
-    
-    # Save the file
-    with open(save_path, "wb") as file:
-        file.write(uploaded_image.getbuffer())
-    
-    st.success(f"Image saved at: {save_path}")
-
-    # # Convert the uploaded image to binary
-    image_data = uploaded_image.read()
-
-    files = {
-        "file": image_data
-    }
-
-    # st.write(f"{os.path.getsize(save_path)}")
-
-    # img body data
-    submit_img_body = {
-        "createdBy": "Joe Holownia",
-        "updatedBy": "Joe Holownia",
-        "sessionId": f"{session_id}",
-        "name": "image_of_food",
-        "plugins": ["plugin-1712327325", "plugin-1713962163"],
-        "sizeBytes":  7834093,
-        "responseMode": "sync"
-    }             
-
-    # submit image to image api
-    try:
-
-        response = requests.post(submit_img_url, 
-                                 headers=submit_img_header, 
-                                 files=files, 
-                                 data=submit_img_body)
-
-        # To print the response
-        # st.write(response.status_code)
-        # st.write(response.json())
-    except Exception as e:
-        st.write("Error:", e)
-
-# Chatbot-like input box with placeholder text
-user_input = st.text_input("", placeholder="")
-
-# --- Processing Input ---
-
-if user_input and uploaded_image:
-
-    # Display the user query
-    st.write(f"User asked: {user_input}")
-
-    # chat query body data
-    submit_query_body = {
-        "endpointId": "predefined-openai-gpt4o",
-        "query": f"{user_input}",
-        "pluginIds": ["plugin-1712327325", "plugin-1713962163"],
-        "responseMode": "sync"
-    }
-
-    try:
-        response = requests.post(submit_query_url,
-                                 headers=submit_query_headers,
-                                 json=submit_query_body
-                                 )
-        if response.status_code == 200:
-            result = response.json()
-            print(result)
-            # Display the chatbot's response
-            st.write("Chatbot response:")
-            st.write(result['data']['answer'])
-    except Exception as e:
-        st.write("Error:", e)
 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 #SIDE BAR
